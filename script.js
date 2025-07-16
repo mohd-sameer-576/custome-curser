d2ddfa59361b3a9d719c03849694e13977a1787c
mohd-sameer-576 <xmohdsameerx576@gmail.com>
2025-07-16 16:01:59 +0530

    Remove comments and clean up cursor styles
    Removed unnecessary comments from script.js for clarity. Simplified the cursor style in style.css by removing a redundant comment.

--- script.js
+++ script.js
@@ -4,7 +4,6 @@ const shadowCurser = document.querySelector('.shadowcurser');
 let mouseX = 0, mouseY = 0;
 let shadowX = 0, shadowY = 0;
 
-// Update mouse position on move
 document.addEventListener('mousemove', (e) => {
     mouseX = e.clientX;
     mouseY = e.clientY;
@@ -12,7 +11,6 @@ document.addEventListener('mousemove', (e) => {
     smallCurser.style.top = `${mouseY - 5}px`;
 });
 
-// Animate shadow to follow cursor with delay
 function animateShadow() {
     shadowX += (mouseX - shadowX) * 0.1;
     shadowY += (mouseY - shadowY) * 0.1;
